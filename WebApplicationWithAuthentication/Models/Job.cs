73d7a88afc8a8cb301a66b40398ffaee43627b42
Rasmus Føgh Sørensen <au618865@uni.au.dk>
2020-03-31 20:22:00 +0200

    Add project files.

---
+++ WebApplicationWithAuthentication/Models/Job.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationWithAuthentication.Models
+{
+    public partial class Job
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Experience { get; set; }
+    }
+}
